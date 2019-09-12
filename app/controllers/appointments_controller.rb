788ed47f6f71da9f2a9e51ade6f1958da9893158
Jennifer Porter <jlporter@myhiddencove.com>
2019-09-12 16:41:57 -0600

    update appt controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,44 +1,54 @@
 class AppointmentsController < ApplicationController
-  before_action :set_appointment, only: [:show]
-  before_action :set_physician
-  before_action :set_patients, only: [:new, :create]
+  before_action :set_appointment, only: [:show]
+  before_action :set_doctor
+  before_action :set_patients, only: [:new, :create]
 
-  def index
-    @appointments = @physician.appointments
+  def index
+    @appointments = @physician.appointments
+  end
+
+  def new
+    @patients = Patient.all - @physician.patients
+    @appointment = @physician.appointments.new
   end
 
   def show
     @patient = @appointment.patient.find(params[:id])
-    @physician = @appointment.physician.find(params[physician_id])
+    @doctor = @appointment.doctor.find(params[:doctor_id])
   end
 
-  def new
-    @patient = Patient.all
-    @appointment = @physician.appointments.new
-  end
-
-  def create
-    @patient = Patient.all
-    @appointment = @physician.appointments.new(appointment_params)
-    if @appointment.save
-      redirect_to physician_appointments_path(@physician)
+  def create
+    @appointment = @physician.appointments.new(appointment_params)
+    if @appointment.save
+      redirect_to physician_appointments_path
     else
-      render :new
+      render :new
     end
   end
 
-  def destroy
-    @appointment = @physician.appointments.find(params[:id])
-    @appointment.destroy
-    redirect_to physician_appointments_path(@physician)
+  def destroy
+    @physician.appointments.find(params[:id]).destroy
+    redirect_to physician_appointments_path
   end
 
- private
-   def set_physician
-     @physician = Physician.find(params[:physician_id]) 
-   end
+private
+  def set_physician
+    @physician = Physician.find(params[:physician_id])
+  end
 
-   def appointment_params
-     params.require(:appointment).permit(patient_url @patient)
-   end
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def set_patients
+    @patients = (Patient.all - @physician.patients)
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:patient_id, :date, :time)
+  end
 end
+  
+
+    
+
